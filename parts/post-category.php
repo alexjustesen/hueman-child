e4bfae97e3e1ba2c2995f43f89ad54203e925376
Alex Justesen <alex@alexjustesen.com>
2017-01-16 16:31:11 -0500

    initial commit

---
+++ parts/post-category.php
@@ -0,0 +1,41 @@
+<?php $category = get_the_category(); ?>
+<?php
+    switch( $category[0]->cat_name ) {
+        case "Galleries" :
+            echo '<p class="post-category hotness"><i class="fa fa-picture-o fa-fw"></i> ';
+            the_category(' / ');
+            echo '</p>';
+            break;
+        case "Hotness" :
+            echo '<p class="post-category hotness"><i class="fa fa-fire fa-fw"></i> ';
+            the_category(' / ');
+            echo '</p>';
+            break;
+        case "Humanity" :
+            echo '<p class="post-category humanity"><i class="fa fa-heart fa-fw"></i> ';
+            the_category(' / ');
+            echo '</p>';
+            break;
+        case "Humor" :
+            echo '<p class="post-category humor"><i class="fa fa-thumbs-up fa-fw"></i> ';
+            the_category(' / ');
+            echo '</p>';
+            break;
+        case "News" :
+            echo '<p class="post-category humor"><i class="fa fa-newspaper-o fa-fw"></i> ';
+            the_category(' / ');
+            echo '</p>';
+            break;
+        case "Poll" :
+        case "Polls" :
+            echo '<p class="post-category poll"><i class="fa fa-bar-chart fa-fw"></i> ';
+            the_category(' / ');
+            echo '</p>';
+            break;
+        default : 
+            echo '<p class="post-category">';
+            the_category(' / ');
+            echo '</p>';
+            break;
+    }
+?>
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Hueman Child Theme
+http://alexjustesen.com
+
+#### Description:
+This theme was designed for WordPress as a child theme for Hueman, use the theme how ever you like it is released under GNU General Public License v2 or later. To install the theme download the repo and add to your themes directory in WordPress.
+
+#### Requirements:
+* WordPress 4.6+
+* Hueman WordPress Theme (https://wordpress.org/themes/hueman/)
+* Hueman Addons WordPress Plugin (https://wordpress.org/plugins/hueman-addons/)
+
+#### Installation:
+1. Download the latest theme package from the theme releases.
+2. Unzip the package and upload to the WordPress themes directory.
+3. Activate the child theme.
+
+#### Change Log:
+
+##### v1.0.0 (Jan 16, 2017)
+- Initial commit.
